b8fdae3a17824d64e84303b7382b00ea8e645be6
Ahmadjon-swe <ahmadovahmadjonqsh125@gmail.com>
2026-04-14 15:24:01 +0500

    added 1-dars

--- 1-dars/src/item/item.controller.ts
+++ 1-dars/src/item/item.controller.ts
@@ -11,36 +11,36 @@ import {
 import { ItemService } from './item.service';
 import { CreateItemDto } from './dto/create-item.dto';
 
-@Controller()
+@Controller('item')
 export class ItemController {
   constructor(private itemService: ItemService) {}
 
   @HttpCode(200)
-  @Get()
+  @Get('get_all_items')
   getItems() {
     return this.itemService.getItems();
   }
 
   @HttpCode(200)
-  @Get()
+  @Get('get_one_item/:id')
   getOneItem(@Param('id') id: string) {
     return this.itemService.getOneItem(+id);
   }
 
   @HttpCode(201)
-  @Patch()
+  @Patch('update_item/:id')
   updateItem(@Param('id') id: string, @Body() item: Partial<CreateItemDto>) {
     return this.itemService.updateItem(+id, item);
   }
 
   @HttpCode(201)
-  @Post()
+  @Post('add_item')
   addItem(@Body() item: CreateItemDto) {
     return this.itemService.addItem(item);
   }
 
   @HttpCode(200)
-  @Delete()
+  @Delete('delete_item/:id')
   deleteItem(@Param('id') id: string) {
     return this.itemService.deleteItem(+id);
   }
--- 1-dars/src/product/product.controller.ts
+++ 1-dars/src/product/product.controller.ts
@@ -15,27 +15,27 @@ import { UpdateProductDto } from './dto/update-product.dto';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Post()
+  @Post('add_product')
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
-  @Get()
+  @Get('get_all_products')
   findAll() {
     return this.productService.findAll();
   }
 
-  @Get(':id')
+  @Get('get_one_product/:id')
   findOne(@Param('id') id: string) {
     return this.productService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch('update_product/:id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(+id, updateProductDto);
   }
 
-  @Delete(':id')
+  @Delete('delete_product/:id')
   remove(@Param('id') id: string) {
     return this.productService.remove(+id);
   }
